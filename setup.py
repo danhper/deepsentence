1d7da5dccf49d8e915fa544f0b08299f02f56166
Daniel Perez <daniel@claudetech.com>
2016-11-25 14:50:54 +0900

    Fix configuration to deploy with scrapyd-deploy

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='deep_sentence',
     version='0.1.0',
-    packages=['deep_sentence'],
+    packages=find_packages(),
+    entry_points={
+        'scrapy': ['settings = deep_sentence.scraper.settings']
+    },
     install_requires=[
         'scrapy',
         'python-dotenv',
