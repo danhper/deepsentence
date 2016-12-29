52e2eda32d21e160f68a97bba72ebc4ec5749ed7
da-tash <daigo_t_1014@yahoo.co.jp>
2016-12-29 12:51:00 +0900

    Add new files for making teacher

---
+++ data/scripts/pyscripts/similarity.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+
+import argparse
+import pickle
+import numpy as np
+import numpy as np
+import scipy
+import pandas as pd
+import MeCab
+from gensim.models import Word2Vec
+
+w2v_path = '../w2v_model/entity_vector/entity_vector.model.bin'
+w2v_model = Word2Vec.load_word2vec_format(w2v_path, binary=True)
+WORD_CLASS = ['名詞', '動詞', '形容詞', '形容動詞', '副詞']
+
+
+def tokenize_POSfilter(sentence):
+    tagger = MeCab.Tagger('-Ochasen')
+    tagger.parse('')
+    node = tagger.parseToNode(sentence)
+    filtered = []
+    while node:
+        if str(node.feature.split(',')[0]) in WORD_CLASS:
+            filtered.append(node.surface)
+        node = node.next
+    return filtered
+
+def tokenize(sentence):
+    mt    = MeCab.Tagger('-Owakati') 
+    parse = mt.parse(sentence)
+    return parse.split()
+
+def word_similarity(word1, word2):
+    try:
+        return w2v_model.similarity(word1, word2)
+    except (KeyError, ValueError):
+        return 0.0
+    
+def sentence_similarity(sentence1, sentence2):
+    '''
+    sentence1: 要約文
+    sentence2: 元記事
+    '''
+    tokens1 = tokenize_POSfilter(sentence1)
+    tokens2 = tokenize_POSfilter(sentence2) 
+    
+    w_scores = []
+    for word1 in tokens1:
+        w_score = 0.0
+        for word2 in tokens2:
+            temp = word_similarity(word1, word2)
+            if temp > w_score:
+                w_score = temp
+                if temp == 1.0:
+                    break
+        w_scores.append(w_score)
+    s_score = np.average(np.array(w_scores))
+
+    return s_score
+    
+
+if __name__ == '__main__':
+    pd.set_option('display.width', 1000)
+    
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('--input_path', type=int, default=None, help='t')
+    args = parser.parse_args()
+
+    summary = '年末年始を海外や古里で過ごす人たちの移動ラッシュがピークを迎え、空港やターミナル駅は29日午前、大きなキャリーバッグを引く旅行者や家族連れらでにぎわいました。'
+    sentence1 = '年末年始を海外や古里で過ごす人たちの移動ラッシュがピークを迎え、空港やターミナル駅は29日午前、大きなキャリーバッグを引く旅行者や家族連れらでにぎわった。'
+    sentence2 = '成田空港では、妻と一緒にハワイで年越しするというさいたま市の会社員関根彦一さんが「きれいな風景を写真に収めたい」と話した。成田国際空港会社によると、29日の出国者数は約5万人の見込み。来年1月3日が帰国ピークとしている。'
+    sentence3 = 'JR東京駅の新幹線ホームでは帰省客が目立った。宮城県の実家に帰省する東京都の主婦、水野一美さんは「7カ月の長男の顔を両親に見せてあげたい。私は少しのんびり過ごせたらいいな」と話した。'
+    
+    print(sentence_similarity(summary, sentence1))
+    print(sentence_similarity(summary, sentence2))
+    print(sentence_similarity(summary, sentence3))
+
+    
