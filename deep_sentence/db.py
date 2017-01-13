53d7cd3ef446237ab2256abb51a8fedb62551253
Daniel Perez <daniel@claudetech.com>
2017-01-13 10:19:37 +0900

    Improve fetch_sources

--- deep_sentence/db.py
+++ deep_sentence/db.py
@@ -6,12 +6,14 @@ from sqlalchemy.orm import sessionmaker
 from . import settings
 
 
-def create_engine():
-    return sqlalchemy.create_engine(settings.DATABASE_URL)
+def create_engine(**kwargs):
+    return sqlalchemy.create_engine(settings.DATABASE_URL, **kwargs)
 
 
-def create_session_maker():
-    engine = create_engine()
+def create_session_maker(engine_options=None):
+    if not engine_options:
+        engine_options = {}
+    engine = create_engine(**engine_options)
     return sessionmaker(bind=engine)
 
 
