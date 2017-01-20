ee067618dfab64025bbe8794c4a439072e57c4ae
Daniel Perez <daniel@claudetech.com>
2017-01-20 18:42:53 +0900

    Add utility to download models

--- deep_sentence/utils/downloader.py
+++ deep_sentence/utils/downloader.py
@@ -7,9 +7,21 @@ import wget
 from deep_sentence import settings
 
 
-def download_embeddings():
-    output_path = path.join(settings.MODELS_PATH, path.basename(settings.WORD_EMBEDDINGS_URL))
-    wget.download(settings.WORD_EMBEDDINGS_URL, output_path)
+def download_tar(url, output_dir):
+    output_path = path.join(output_dir, path.basename(url))
+    wget.download(url, output_path)
     compressed = tarfile.open(output_path)
-    compressed.extractall(settings.MODELS_PATH)
+    compressed.extractall(output_dir)
     os.remove(output_path)
+
+
+def download_embeddings():
+    download_tar(settings.WORD_EMBEDDINGS_URL, settings.MODELS_PATH)
+
+
+def download_abstractive_data():
+    download_tar(settings.ABSTRACTIVE_DATA_URL, path.join(settings.MODELS_PATH, 'abstractive'))
+
+
+def download_abstractive_trained():
+    download_tar(settings.ABSTRACTIVE_TRAINED_URL, path.join(settings.MODELS_PATH, 'abstractive'))
